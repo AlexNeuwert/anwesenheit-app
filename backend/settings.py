61fa2b45119443b83f754257d6fd09b4ba7aec24
AlexNeuwert <alexandra.neuwert@ibb-dresden.de>
2026-09-18 12:06:18 +0200

    Setting für Deployment ergänzt

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
     ]
-
+CSRF_TRUSTED_ORIGINS = [
+    "https://grundschule.ibb-dresden.de",
+    ]
 
 # Application definition
 
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
