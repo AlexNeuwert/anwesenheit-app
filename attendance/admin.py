ebb93a0e14debf5383dbb23d7c8614c7143a9fed
AlexNeuwert <alexandra.neuwert@ibb-dresden.de>
2026-09-21 11:56:56 +0200

    Suchfenster bei Django Admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -2,7 +2,15 @@
 from django.contrib import admin
 from .models import Student, Attendance
 
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = [
+        "name",
+        "student_id",
+        "student_class",
+    ]
+
+
 admin.site.register(Attendance)
 
 
